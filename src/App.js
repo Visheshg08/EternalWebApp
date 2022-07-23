4f3054201ceff094dd73f60441ce896fb9902c71
Visheshg08 <visheshgoyal0804@gmail.com>
2022-07-23 20:47:47 +0530

    about changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  return (null);
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
