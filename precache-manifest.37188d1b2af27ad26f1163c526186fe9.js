27161790cdd6e7200aac7c6d91ff20514417b910
Visheshg08 <visheshgoyal0804@gmail.com>
2022-08-09 00:46:20 +0530

    Updates

---
+++ precache-manifest.37188d1b2af27ad26f1163c526186fe9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e29d6b04cb73aa33de21",
+    "url": "/static/css/main.eb3308cc.chunk.css"
+  },
+  {
+    "revision": "e29d6b04cb73aa33de21",
+    "url": "/static/js/main.e29d6b04.chunk.js"
+  },
+  {
+    "revision": "b4036f731a649b9452fb",
+    "url": "/static/js/1.b4036f73.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4a4dfb4e7eb6e4f6c67ec6140252318a",
+    "url": "/index.html"
+  }
+];
